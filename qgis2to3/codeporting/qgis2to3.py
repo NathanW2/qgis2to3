00ab0cb7cce6308ed335aea78964903a02072931
Nathan Woodrow <nathan_woodrow@technologyonecorp.com>
2018-10-02 15:46:36 +1000

    Fix - Don't change line endings

--- qgis2to3/codeporting/qgis2to3.py
+++ qgis2to3/codeporting/qgis2to3.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import sys, os
+import functools
 import qgis2to3.codeporting
 
 import lib2to3.main
 
+import lib2to3.refactor
+
+lib2to3.refactor._open_with_encoding = functools.partial(open, newline='')
+
 def main():
     sys.path.append(os.path.abspath(os.path.dirname(qgis2to3.codeporting.__file__)))
-
     sys.exit(lib2to3.main.main('qgis_fixes'))
 
 if __name__ == "__main__":
